3f5d360a6e5a41dfd92069375c6f4ed00f5618c8
Nanfack95 <nanfackjospinduclair@gmail.com>
2025-07-05 12:43:02 +0100

    connexion front back notification Reinitialistion mot de passe + validation donnees formulaire

--- src/auth.jsx
+++ src/auth.jsx
@@ -9,3 +9,9 @@ export const loginUser = async (data) => {
   const response = await api.post('/login', data);
   return response.data;
 };
+
+export const sendPasswordResetLink = async (email) => {
+  const res = await api.post('/forgot-password', { email });
+  return res.data;
+};
+
