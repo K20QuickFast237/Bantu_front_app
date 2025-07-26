47358e0ac3f78584cff90b5bb46d5ab1896750e6
Nanfack95 <nanfackjospinduclair@gmail.com>
2025-07-26 17:01:53 +0100

    Protection des routes privees

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,10 +1,16 @@
-import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { AuthProvider } from "../context/AuthContext";
+import { useAuth } from "../hooks/useAuth";
 
 const PrivateRoutes = () => {
-  const { isAuthenticated } = useContext(AuthProvider);
+  // On récupère le token depuis notre contexte d'authentification via le hook.
+  const { token } = useAuth();
 
+  // C'est la vérification la plus simple et la plus fiable.
+  // `!!token` convertit la chaîne de caractères (ou null) en un booléen.
+  const isAuthenticated = !!token;
+
+  // Si l'utilisateur est authentifié, on affiche le contenu de la route enfant (la page protégée).
+  // Si l'utilisateur n'est pas authentifié, on le redirige vers la page de connexion.
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
